b35ef5aae980a154a52fabf280192396e99d3a5f
simaipeter <simaipeter@gmail.com>
2022-06-23 19:25:03 +0200

    0.1

---
+++ src/main/java/hu/nye/algoritmusok_tervezese/Itinerary_alternatives/cityData.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hu.nye.algoritmusok_tervezese.Itinerary_alternatives;
+
+/**
+ *
+ * @author x
+ */
+class cityData {
+   
+    public int id;
+    public static String fromCity;
+    public static String toCity;
+    public int transferCount;
+    public int time;
+
+    public cityData() {
+        
+    }
+ 
+    
+}
